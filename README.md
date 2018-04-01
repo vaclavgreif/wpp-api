e3e03caef125d4a648f3c6cf8b1cc2ec1da292fb
Václav Greif <vaclav.greif@gmail.com>
2018-04-01 18:38:55 +0200

    Added an example

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Wordpress API wrapper
 A wrapper around Wordpress remote_request() method, to make sending and receiving requests from Wordpress to external services easier
+
+## Usage
+Check the examples folder for a basic example of how this can make your life easier.
---
+++ examples/class.test_api.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Test API example
+ */
+namespace Test_API;
+use WPProgramator\Api;
+
+class TestMethod extends Api {
+
+	/**
+	 * TestMethod constructor.
+	 * See https://developer.wordpress.org/reference/classes/WP_Http/request/ for the possible args
+	 */
+	function __construct() {
+		// Set the endpoing
+		$this->set_endpoint('https://myapiurl.com');
+
+		// Set the API args
+
+		$this->set_args(array(
+			'timeout' => 45,
+			'redirection' => 5,
+			'httpversion' => '1.0',
+			'blocking' => true,
+			'headers' => array(),
+			'body' => array( 'username' => 'bob', 'password' => '1234xyz' ),
+			'cookies' => array()
+		));
+
+		parent::__construct( );
+	}
+
+	/**
+	 * Example function to get some data from example API
+	 * @return array|bool|mixed|object|\WP_Error
+	 */
+	function get_some_data() {
+		$args['body'] = [
+			'some_param' => 'some_value',
+			'another_param' => 'another_value'
+		];
+
+		$this->add_args($args);
+		$this->set_endpoint('some-endpoint');
+
+		return $this->_get();
+	}
+}
+
+$api_method = new TestMethod();
+$result = $api_method->get_some_data();
+
+// Do something with the result
